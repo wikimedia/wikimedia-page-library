9eb50e23420cdf0c76ca9ff5bd60f14aaaf1ab3b
Stephen Niedzielski <stephen@niedzielski.com>
2017-02-18 21:56:50 -0700

    New: copy getTableHeader() from the Wikipedia Android app
    Copy getTableHeader() function verbatim from
    www/js/transforms/collapseTables.js of apps-android-wikipedia:781d149

---
+++ src/CollapseElement.js
@@ -0,0 +1,36 @@
+/*
+Tries to get an array of table header (TH) contents from a given table.
+If there are no TH elements in the table, an empty array is returned.
+*/
+function getTableHeader( element ) {
+    var thArray = [];
+    if (element.children === undefined || element.children === null) {
+        return thArray;
+    }
+    for (var i = 0; i < element.children.length; i++) {
+        var el = element.children[i];
+        if (el.tagName === "TH") {
+            // ok, we have a TH element!
+            // However, if it contains more than two links, then ignore it, because
+            // it will probably appear weird when rendered as plain text.
+            var aNodes = el.querySelectorAll( "a" );
+            if (aNodes.length < 3) {
+                // Also ignore it if it's identical to the page title.
+                if (el.innerText.length > 0 && el.innerText !== window.pageTitle && el.innerHTML !== window.pageTitle) {
+                    thArray.push(el.innerText);
+                }
+            }
+        }
+        //if it's a table within a table, don't worry about it
+        if (el.tagName === "TABLE") {
+            continue;
+        }
+        //recurse into children of this element
+        var ret = getTableHeader(el);
+        //did we get a list of TH from this child?
+        if (ret.length > 0) {
+            thArray = thArray.concat(ret);
+        }
+    }
+    return thArray;
+}
