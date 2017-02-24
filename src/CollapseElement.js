62f41a376dc03d1e6bff7a9247fa90f70b77494d
Stephen Niedzielski <stephen@niedzielski.com>
2017-02-24 15:44:20 -0700

    Chore: update ESLint config to require single quotes

--- src/CollapseElement.js
+++ src/CollapseElement.js
@@ -7,18 +7,21 @@
 */
 export const getTableHeader = (element, pageTitle) => {
   let thArray = []
+
   if (element.children === undefined || element.children === null) {
     return thArray
   }
+
   for (let i = 0; i < element.children.length; i++) {
     const el = element.children[i]
-    if (el.tagName === "TH") {
-            // ok, we have a TH element!
-            // However, if it contains more than two links, then ignore it, because
-            // it will probably appear weird when rendered as plain text.
-      const aNodes = el.querySelectorAll("a")
+
+    if (el.tagName === 'TH') {
+      // ok, we have a TH element!
+      // However, if it contains more than two links, then ignore it, because
+      // it will probably appear weird when rendered as plain text.
+      const aNodes = el.querySelectorAll('a')
       if (aNodes.length < 3) {
-                // Also ignore it if it's identical to the page title.
+        // Also ignore it if it's identical to the page title.
         if ((el.innerText && el.innerText.length || el.textContent.length) > 0
                 && el.innerText !== pageTitle && el.textContent !== pageTitle
                 && el.innerHTML !== pageTitle) {
@@ -26,16 +29,20 @@ export const getTableHeader = (element, pageTitle) => {
         }
       }
     }
-        // if it's a table within a table, don't worry about it
-    if (el.tagName === "TABLE") {
+
+    // if it's a table within a table, don't worry about it
+    if (el.tagName === 'TABLE') {
       continue
     }
-        // recurse into children of this element
+
+    // recurse into children of this element
     const ret = getTableHeader(el, pageTitle)
-        // did we get a list of TH from this child?
+
+    // did we get a list of TH from this child?
     if (ret.length > 0) {
       thArray = thArray.concat(ret)
     }
   }
+
   return thArray
 }
