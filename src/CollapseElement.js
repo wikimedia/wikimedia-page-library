567fcafd41def36d804d3769dcdb828bf10efa7e
Stephen Niedzielski <stephen@niedzielski.com>
2017-02-24 12:58:18 -0700

    Chore: add ESLint config

--- src/CollapseElement.js
+++ src/CollapseElement.js
@@ -1,6 +1,8 @@
 // This function preserves the original functionality
-function getTableHeaderTmp( element ) {
-    return window.pageTitle( element, window.pageTitle )
+// todo: move to client
+const getTableHeaderTmp = (element) => { // eslint-disable-line no-unused-vars
+  /* global window */
+  return window.pageTitle(element, window.pageTitle)
 }
 
 /**
@@ -10,42 +12,42 @@ function getTableHeaderTmp( element ) {
   @param {?string} pageTitle
   @return {!Array<string>}
 */
-function getTableHeader( element, pageTitle ) {
-    var thArray = [];
-    if (element.children === undefined || element.children === null) {
-        return thArray;
-    }
-    for (var i = 0; i < element.children.length; i++) {
-        var el = element.children[i];
-        if (el.tagName === "TH") {
+const getTableHeader = (element, pageTitle) => {
+  let thArray = []
+  if (element.children === undefined || element.children === null) {
+    return thArray
+  }
+  for (let i = 0; i < element.children.length; i++) {
+    const el = element.children[i]
+    if (el.tagName === "TH") {
             // ok, we have a TH element!
             // However, if it contains more than two links, then ignore it, because
             // it will probably appear weird when rendered as plain text.
-            var aNodes = el.querySelectorAll( "a" );
-            if (aNodes.length < 3) {
+      const aNodes = el.querySelectorAll("a")
+      if (aNodes.length < 3) {
                 // Also ignore it if it's identical to the page title.
-                if ((el.innerText && el.innerText.length || el.textContent.length) > 0
-                && el.innerText !== pageTitle && el.textContent !== pageTitle && el.innerHTML !== pageTitle) {
-                    thArray.push(el.innerText || el.textContent);
-                }
-            }
+        if ((el.innerText && el.innerText.length || el.textContent.length) > 0
+                && el.innerText !== pageTitle && el.textContent !== pageTitle
+                && el.innerHTML !== pageTitle) {
+          thArray.push(el.innerText || el.textContent)
         }
-        //if it's a table within a table, don't worry about it
-        if (el.tagName === "TABLE") {
-            continue;
-        }
-        //recurse into children of this element
-        var ret = getTableHeader(el, pageTitle);
-        //did we get a list of TH from this child?
-        if (ret.length > 0) {
-            thArray = thArray.concat(ret);
-        }
+      }
     }
-    return thArray;
+        // if it's a table within a table, don't worry about it
+    if (el.tagName === "TABLE") {
+      continue
+    }
+        // recurse into children of this element
+    const ret = getTableHeader(el, pageTitle)
+        // did we get a list of TH from this child?
+    if (ret.length > 0) {
+      thArray = thArray.concat(ret)
+    }
+  }
+  return thArray
 }
 
-// todo: eslint
 // todo: use import and export
 module.exports = {
-    getTableHeader
+  getTableHeader
 }
--- test/CollapseElement.js
+++ test/CollapseElement.js
@@ -7,25 +7,25 @@ describe('CollapseElement', () => {
   describe('getTableHeader()', () => {
     const getTableHeader = CollapseElement.getTableHeader
 
-    it('no table', function() {
+    it('no table', () => {
       const doc = domino.createDocument('<html></html>')
       const actual = getTableHeader(doc.documentElement)
       assert.deepEqual([], actual)
     })
 
-    it('no header', function() {
+    it('no header', () => {
       const doc = domino.createDocument('<table></table>')
       const actual = getTableHeader(doc.querySelector('table'))
       assert.deepEqual([], actual)
     })
 
-    it('empty header', function() {
+    it('empty header', () => {
       const doc = domino.createDocument('<table><tr><th></th></tr></table>')
       const actual = getTableHeader(doc.querySelector('table'))
       assert.deepEqual([], actual)
     })
 
-    it('nonempty header', function() {
+    it('nonempty header', () => {
       const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
       const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
       assert.deepEqual(['text'], actual)
