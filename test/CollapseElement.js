eefb717ca5648f3dc2cd05e0494180eb45679a79
Stephen Niedzielski <stephen@niedzielski.com>
2017-02-24 15:32:52 -0700

    Chore: add more CollapseElement.getTableHeader() tests

--- test/CollapseElement.js
+++ test/CollapseElement.js
@@ -6,19 +6,19 @@ describe('CollapseElement', () => {
   describe('getTableHeader()', () => {
     it('no table', () => {
       const doc = domino.createDocument('<html></html>')
-      const actual = getTableHeader(doc.documentElement)
+      const actual = getTableHeader(doc.documentElement, 'pageTitle')
       assert.deepEqual(actual, [])
     })
 
     it('no header', () => {
       const doc = domino.createDocument('<table></table>')
-      const actual = getTableHeader(doc.querySelector('table'))
+      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
       assert.deepEqual(actual, [])
     })
 
     it('empty header', () => {
       const doc = domino.createDocument('<table><tr><th></th></tr></table>')
-      const actual = getTableHeader(doc.querySelector('table'))
+      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
       assert.deepEqual(actual, [])
     })
 
@@ -26,6 +26,24 @@ describe('CollapseElement', () => {
       const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
       const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
       assert.deepEqual(actual, ['text'])
+    })
+
+    it('empty link', () => {
+      const doc = domino.createDocument('<table><tr><th><a></a></th></tr></table>')
+      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+      assert.deepEqual(actual, [])
+    })
+
+    it('matching link', () => {
+      const doc = domino.createDocument('<table><tr><th><a>pageTitle</a></th></tr></table>')
+      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+      assert.deepEqual(actual, [])
+    })
+
+    it('no page title', () => {
+      const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
+      const actual = getTableHeader(doc.querySelector('table'))
+      assert.deepEqual(actual, [])
     })
   })
 })
