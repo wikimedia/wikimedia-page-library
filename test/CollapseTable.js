18c7fac4c758b04c95400e9f4bec411a34f95e3c
Stephen Niedzielski <stephen@niedzielski.com>
2017-03-17 11:49:40 -0600

    Chore: add CollapseTable.toggleCollapseClickCallback() tests

--- test/CollapseTable.js
+++ test/CollapseTable.js
@@ -48,4 +48,104 @@ describe('CollapseTable', () => {
       assert.deepEqual(actual, [])
     })
   })
+
+  describe('.toggleCollapseClickCallback()', () => {
+    const toggleCollapseClickCallback = applib.CollapseTable.toggleCollapseClickCallback
+
+    describe('and an open container', () => {
+      beforeEach(function Test() {
+        const html = `<div>
+                        <div
+                          id=header
+                          class='app_table_collapse_close app_table_collapse_icon'
+                          style='visibility: visible'>
+                          <span
+                            id=caption
+                            class=app_table_collapsed_caption
+                            style='visibility: hidden'
+                          </span>
+                        </div>
+                        <table style='display: block'></table>
+                        <div id=footer style='display: block'></div>
+                      </div>`
+        this.doc = domino.createDocument(html)
+      })
+
+      it('where the footer is observed, the callback is invoked when the footer is clicked',
+        function Test(done) {
+          const footer = this.doc.querySelector('#footer')
+          footer.addEventListener('click',
+            toggleCollapseClickCallback.bind(footer, () => { done() }))
+          footer.click()
+        })
+
+      describe('where the header is observed,', () => {
+        beforeEach(function Test() {
+          this.header = this.doc.querySelector('#header')
+          this.header.addEventListener('click',
+            toggleCollapseClickCallback.bind(this.header, () => {}))
+        })
+
+        it('the callback is not invoked when the header clicked', function Test() {
+          this.header.addEventListener('click',
+            toggleCollapseClickCallback.bind(this.header, () => { assert.fail() }))
+          this.header.click()
+        })
+
+        it('the header class list toggles when clicked', function Test() {
+          this.header.click()
+          assert.ok(this.header.classList.contains('app_table_collapsed_open'))
+          assert.ok(!this.header.classList.contains('app_table_collapse_close'))
+          assert.ok(!this.header.classList.contains('app_table_collapse_icon'))
+        })
+
+        it('the header class list toggles back when clicked twice', function Test() {
+          this.header.click()
+          this.header.click()
+          assert.ok(!this.header.classList.contains('app_table_collapsed_open'))
+          assert.ok(this.header.classList.contains('app_table_collapse_close'))
+          assert.ok(this.header.classList.contains('app_table_collapse_icon'))
+        })
+
+        it('the caption is shown when clicked', function Test() {
+          this.header.click()
+          const caption = this.doc.querySelector('#caption')
+          assert.deepEqual(caption.style.visibility, 'visible')
+        })
+
+        it('the caption is hidden when clicked twice', function Test() {
+          this.header.click()
+          this.header.click()
+          const caption = this.doc.querySelector('#caption')
+          assert.deepEqual(caption.style.visibility, 'hidden')
+        })
+
+        it('the table is hidden when clicked', function Test() {
+          this.header.click()
+          const table = this.doc.querySelector('table')
+          assert.deepEqual(table.style.display, 'none')
+        })
+
+        it('the table is shown when clicked twice', function Test() {
+          this.header.click()
+          this.header.click()
+          const table = this.doc.querySelector('table')
+          assert.deepEqual(table.style.display, 'block')
+        })
+
+        it('the footer is hidden when clicked', function Test() {
+          this.header.click()
+          const footer = this.doc.querySelector('#footer')
+          assert.deepEqual(footer.style.display, 'none')
+        })
+
+        it('the footer is shown when clicked twice', function Test() {
+          this.header.click()
+          this.header.click()
+          const footer = this.doc.querySelector('#footer')
+          assert.deepEqual(footer.style.display, 'block')
+        })
+      })
+    })
+  })
 })
