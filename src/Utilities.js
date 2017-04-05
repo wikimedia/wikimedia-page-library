4013effcc07dcd715ef53e6bbdfaa281b4e432dc
montehurd <montehurd@gmail.com>
2017-04-04 18:25:04 -0700

    Remove some blank opening newlines.

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,4 +1,3 @@
-
 // Implementation of https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
 const findClosest = (el, selector) => {
   while ((el = el.parentElement) && !el.matches(selector)) {
