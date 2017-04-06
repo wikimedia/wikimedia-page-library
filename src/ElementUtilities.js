abe665895d25bf4451066957c6a546b5b09248ba
montehurd <montehurd@gmail.com>
2017-04-06 14:14:15 -0700

    Add JSDocs to ElementUtilities.

--- src/ElementUtilities.js
+++ src/ElementUtilities.js
@@ -1,4 +1,12 @@
-// Implementation of https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+/**
+ * Returns closest ancestor of element which matches selector.
+ * Similar to 'closest' methods as seen here:
+ *  https://api.jquery.com/closest/
+ *  https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+ * @param  {!HTMLElement} el    Element
+ * @param  {!string} selector   Selector to look for in ancestors of 'el'
+ * @return {?HTMLElement}       Closest ancestor of 'el' matching 'selector'
+ */
 const findClosest = (el, selector) => {
   while ((el = el.parentElement) && !el.matches(selector)) {
     // Intentionally empty.
@@ -6,6 +14,11 @@ const findClosest = (el, selector) => {
   return el
 }
 
+/**
+ * Determines if element has a table ancestor.
+ * @param  {!HTMLElement}  el   Element
+ * @return {Boolean}            Whether table ancestor of 'el' is found
+ */
 const isNestedInTable = (el) => {
   return (findClosest(el, 'table') !== null)
 }
