2fa6cc7a19bc4f5a907f39e4c4269a1253bfa33e
montehurd <montehurd@gmail.com>
2017-04-06 14:21:28 -0700

    Add JSDocs to FixtureIO.

--- test/utilities/FixtureIO.js
+++ test/utilities/FixtureIO.js
@@ -2,10 +2,20 @@ import domino from 'domino'
 import fs from 'fs'
 import path from 'path'
 
+/**
+ * Gets string from file in 'test/fixtures/'
+ * @param  {!string} fileName   Name of file in 'test/fixtures/'
+ * @return {?string}            String from `test/fixtures/${fileName}`
+ */
 const stringFromFixtureFile = (fileName) => {
   return fs.readFileSync(path.resolve(__dirname, `../fixtures/${fileName}`), 'utf8')
 }
 
+/**
+ * Gets Domino document from file in 'test/fixtures/'
+ * @param  {!string} fileName   Name of file in 'test/fixtures/'
+ * @return {?document}          Domino document from `test/fixtures/${fileName}`
+ */
 const documentFromFixtureFile = (fileName) => {
   return domino.createDocument(stringFromFixtureFile(fileName))
 }
