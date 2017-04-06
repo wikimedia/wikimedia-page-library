1485daa293d48aa5e7e55dfa95b8b60b86632f85
montehurd <montehurd@gmail.com>
2017-04-05 18:29:27 -0700

    Add tests for ElementUtilities.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import CollapseTable from './CollapseTable'
+import ElementUtilities from './ElementUtilities'
 import WidenImage from './WidenImage'
 
 export default {
   CollapseTable,
-  WidenImage
+  WidenImage,
+  test: {
+    ElementUtilities
+  }
 }
