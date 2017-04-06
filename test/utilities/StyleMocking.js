9544668c27488b54a36f4b68138e4e1d1e3a1780
montehurd <montehurd@gmail.com>
2017-04-06 14:57:41 -0700

    Add JSDocs to StyleMocking.

--- test/utilities/StyleMocking.js
+++ test/utilities/StyleMocking.js
@@ -1,14 +1,30 @@
 import assert from 'assert'
 
-// Domino doesn't seem to slurp up css class properties if we set them in our fixture html.
-// These methods let us manually set and verify css style properties of element(s) we want to test.
+// These methods are useful in tests which verify changes a method makes to element 'style'
+// properties.
 
+/**
+ * An object whose properties are taken to be style property names,
+ * and whose property values are taken to be style property values.
+ * @typedef {Object} StylesHash
+ */
+
+/**
+ * Sets styles on an element.
+ * @param  {!HTMLElement} element   Element
+ * @param  {!StylesHash}  styles    Styles to apply to 'element'
+ */
 const mockStylesInElement = (element, styles) => {
   for (const [key, value] of Object.entries(styles)) {
     element.style[key] = value
   }
 }
 
+/**
+ * Verifies styles on an element.
+ * @param  {!HTMLElement} element   Element
+ * @param  {!StylesHash}  styles    Styles to verify on 'element'
+ */
 const verifyStylesInElement = (element, styles) => {
   for (const [key, value] of Object.entries(styles)) {
     const msg = `'${key}' was expected to be '${value}' but was '${element.style[key]}'`
@@ -16,12 +32,22 @@ const verifyStylesInElement = (element, styles) => {
   }
 }
 
+/**
+ * Sets styles on each element in an array of elements.
+ * @param  {!Array<HTMLElement>}  elements  Array of elements
+ * @param  {!StylesHash}          styles    Styles to apply to each element in 'elements'
+ */
 const mockStylesInElements = (elements, styles) => {
   for (const element of elements) {
     mockStylesInElement(element, styles)
   }
 }
 
+/**
+ * Verifies styles on each element in an array of elements.
+ * @param  {!Array<HTMLElement>}  elements  Array of elements
+ * @param  {!StylesHash}          styles    Styles to verify on each element in 'elements'
+ */
 const verifyStylesInElements = (elements, styles) => {
   for (const element of elements) {
     verifyStylesInElement(element, styles)
