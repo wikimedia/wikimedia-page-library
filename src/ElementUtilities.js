d38bc30906486ae959c4f73dea3ff6c4ecba8c82
montehurd <montehurd@gmail.com>
2017-04-06 16:05:46 -0700

    Moar JSDoc tweaks.

--- src/ElementUtilities.js
+++ src/ElementUtilities.js
@@ -3,7 +3,7 @@
  * Similar to 'closest' methods as seen here:
  *  https://api.jquery.com/closest/
  *  https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
- * @param  {!HTMLElement} el    Element
+ * @param  {!Element} el        Element
  * @param  {!string} selector   Selector to look for in ancestors of 'el'
  * @return {?HTMLElement}       Closest ancestor of 'el' matching 'selector'
  */
@@ -17,8 +17,8 @@ const findClosest = (el, selector) => {
 
 /**
  * Determines if element has a table ancestor.
- * @param  {!HTMLElement}  el   Element
- * @return {Boolean}            Whether table ancestor of 'el' is found
+ * @param  {!Element}  el   Element
+ * @return {boolean}        Whether table ancestor of 'el' is found
  */
 const isNestedInTable = (el) => {
   return (findClosest(el, 'table') !== null)
