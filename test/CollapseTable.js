962d8ddd2c556dcfdc4833b9643d055d1a6a9792
Stephen Niedzielski <stephen@niedzielski.com>
2017-04-11 11:23:48 -0600

    Chore: make CollapseTable tests more vebose

--- test/CollapseTable.js
+++ test/CollapseTable.js
@@ -6,46 +6,52 @@ describe('CollapseTable', () => {
   describe('getTableHeader()', () => {
     const getTableHeader = applib.CollapseTable.getTableHeader
 
-    it('no table', () => {
+    it('when no table, shouldn\'t find headers', () => {
       const doc = domino.createDocument('<html></html>')
       const actual = getTableHeader(doc.documentElement, 'pageTitle')
       assert.deepEqual(actual, [])
     })
 
-    it('no header', () => {
-      const doc = domino.createDocument('<table></table>')
-      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
-      assert.deepEqual(actual, [])
-    })
+    describe('when table', () => {
+      it('and no header, shouldn\'t find headers', () => {
+        const doc = domino.createDocument('<table></table>')
+        const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+        assert.deepEqual(actual, [])
+      })
 
-    it('empty header', () => {
-      const doc = domino.createDocument('<table><tr><th></th></tr></table>')
-      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
-      assert.deepEqual(actual, [])
-    })
+      it('and header is empty, shouldn\'t find headers', () => {
+        const doc = domino.createDocument('<table><tr><th></th></tr></table>')
+        const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+        assert.deepEqual(actual, [])
+      })
 
-    it('nonempty header', () => {
-      const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
-      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
-      assert.deepEqual(actual, ['text'])
-    })
+      describe('and header is nonempty', () => {
+        it('and link is empty, shouldn\'t find header', () => {
+          const doc = domino.createDocument('<table><tr><th><a></a></th></tr></table>')
+          const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+          assert.deepEqual(actual, [])
+        })
 
-    it('empty link', () => {
-      const doc = domino.createDocument('<table><tr><th><a></a></th></tr></table>')
-      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
-      assert.deepEqual(actual, [])
-    })
+        describe('and link is nonempty', () => {
+          it('and doesn\'t match page title, should find header', () => {
+            const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
+            const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+            assert.deepEqual(actual, ['text'])
+          })
 
-    it('matching link', () => {
-      const doc = domino.createDocument('<table><tr><th><a>pageTitle</a></th></tr></table>')
-      const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
-      assert.deepEqual(actual, [])
-    })
+          it('and matches page title, shouldn\'t find header', () => {
+            const doc = domino.createDocument('<table><tr><th><a>pageTitle</a></th></tr></table>')
+            const actual = getTableHeader(doc.querySelector('table'), 'pageTitle')
+            assert.deepEqual(actual, [])
+          })
 
-    it('no page title', () => {
-      const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
-      const actual = getTableHeader(doc.querySelector('table'))
-      assert.deepEqual(actual, [])
+          it('and no page title, shouldn\'t find header', () => {
+            const doc = domino.createDocument('<table><tr><th><a>text</a></th></tr></table>')
+            const actual = getTableHeader(doc.querySelector('table'))
+            assert.deepEqual(actual, [])
+          })
+        })
+      })
     })
   })
 })
