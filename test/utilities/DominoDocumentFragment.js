4aa49427f60b41018550718b6dbcccc21f3e79ce
montehurd <montehurd@gmail.com>
2017-06-05 14:51:22 -0700

    Use test file naming convention on domino doc frag method.

--- test/utilities/DominoDocumentFragment.js
+++ test/utilities/DominoDocumentFragment.js
@@ -1,4 +1,3 @@
-import assert from 'assert'
 import domino from 'domino'
 
 /**
@@ -14,30 +13,6 @@ const documentFragmentFromHTMLString = HTMLString => {
   const fragment = template.content
   return fragment
 }
-
-describe('DominoDocumentFragment', () => {
-  describe('documentFragmentFromHTMLString()', () => {
-    it('should return a Domino DocumentFragment when given an HTML string', () => {
-      const frag =
-        documentFragmentFromHTMLString('<div>Oh look! A fragment!</div><a id="link1"></a>')
-      assert.ok(frag instanceof domino.impl.DocumentFragment)
-    })
-    it('should return a Domino DocumentFragment with expected childNodes', () => {
-      const frag =
-        documentFragmentFromHTMLString('<div>Oh look! A fragment!</div><a id="link1"></a>')
-      assert.ok(frag.childNodes.length === 2)
-      const firstChild = frag.childNodes[0]
-      const secondChild = frag.childNodes[1]
-      assert.ok(firstChild.tagName === 'DIV')
-      assert.ok(secondChild.tagName === 'A')
-    })
-    it('should not see unexpected method in Domino DocumentFragment', () => {
-      const frag =
-        documentFragmentFromHTMLString('<div>Oh look! A fragment!</div><a id="link1"></a>')
-      assert.ok(frag.createElement === undefined)
-    })
-  })
-})
 
 export default {
   documentFragmentFromHTMLString
---
+++ test/utilities/DominoDocumentFragment.test.js
@@ -0,0 +1,29 @@
+import assert from 'assert'
+import domino from 'domino'
+import dominoDocumentFragment from './DominoDocumentFragment'
+
+const documentFragmentFromHTMLString = dominoDocumentFragment.documentFragmentFromHTMLString
+
+describe('DominoDocumentFragment', () => {
+  describe('documentFragmentFromHTMLString()', () => {
+    it('should return a Domino DocumentFragment when given an HTML string', () => {
+      const frag =
+        documentFragmentFromHTMLString('<div>Oh look! A fragment!</div><a id="link1"></a>')
+      assert.ok(frag instanceof domino.impl.DocumentFragment)
+    })
+    it('should return a Domino DocumentFragment with expected childNodes', () => {
+      const frag =
+        documentFragmentFromHTMLString('<div>Oh look! A fragment!</div><a id="link1"></a>')
+      assert.ok(frag.childNodes.length === 2)
+      const firstChild = frag.childNodes[0]
+      const secondChild = frag.childNodes[1]
+      assert.ok(firstChild.tagName === 'DIV')
+      assert.ok(secondChild.tagName === 'A')
+    })
+    it('should not see unexpected method in Domino DocumentFragment', () => {
+      const frag =
+        documentFragmentFromHTMLString('<div>Oh look! A fragment!</div><a id="link1"></a>')
+      assert.ok(frag.createElement === undefined)
+    })
+  })
+})
