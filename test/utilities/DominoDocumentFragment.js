4e2815835694c5bc2baf581fcfb83c3cedc63223
montehurd <montehurd@gmail.com>
2017-06-13 13:53:55 -0700

    More capitalization and doc tweaks.

--- test/utilities/DominoDocumentFragment.js
+++ test/utilities/DominoDocumentFragment.js
@@ -1,10 +1,10 @@
 import domino from 'domino'
 
 /**
- * Gets a document fragment suitable for use in unit tests
+ * Gets a document fragment suitable for use in unit tests.
  * Details: https://github.com/fgnass/domino/issues/73#issuecomment-200466430
- * @param {?String} HTMLString An html string
- * @return {!domino.impl.DocumentFragment} A domino implementation of a DocumentFragment
+ * @param {?String} HTMLString An HTML string.
+ * @return {!domino.impl.DocumentFragment} A domino implementation of a DocumentFragment.
  */
 const documentFragmentFromHTMLString = HTMLString => {
   const document = domino.createDocument()
