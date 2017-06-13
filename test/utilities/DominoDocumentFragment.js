1166450dd0325b254d784a185e06e6ea2f22f59e
montehurd <montehurd@gmail.com>
2017-06-13 13:55:36 -0700

    MOAR capitalizion tweaks.

--- test/utilities/DominoDocumentFragment.js
+++ test/utilities/DominoDocumentFragment.js
@@ -3,13 +3,13 @@ import domino from 'domino'
 /**
  * Gets a document fragment suitable for use in unit tests.
  * Details: https://github.com/fgnass/domino/issues/73#issuecomment-200466430
- * @param {?String} HTMLString An HTML string.
+ * @param {?String} htmlString An HTML string.
  * @return {!domino.impl.DocumentFragment} A domino implementation of a DocumentFragment.
  */
-const documentFragmentFromHTMLString = HTMLString => {
+const documentFragmentFromHTMLString = htmlString => {
   const document = domino.createDocument()
   const template = document.createElement('template')
-  template.innerHTML = HTMLString
+  template.innerHTML = htmlString
   const fragment = template.content
   return fragment
 }
