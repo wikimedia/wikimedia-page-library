49d972b3ad5475e60d8eafb77f944b0aa4258be2
montehurd <montehurd@gmail.com>
2017-06-13 13:38:50 -0700

    Add periods to JSDocs.

--- src/transform/RedLinks.js
+++ src/transform/RedLinks.js
@@ -1,7 +1,7 @@
 /**
- * Configures span to be suitable replacement for redLink anchor
- * @param {!HTMLSpanElement} span The span element to configure as anchor replacement
- * @param {!HTMLAnchorElement} anchor The anchor element being replaced
+ * Configures span to be suitable replacement for redLink anchor.
+ * @param {!HTMLSpanElement} span The span element to configure as anchor replacement.
+ * @param {!HTMLAnchorElement} anchor The anchor element being replaced.
  * @return {void}
  */
 const configureSpanAsAnchorReplacement = (span, anchor) => {
@@ -10,35 +10,35 @@ const configureSpanAsAnchorReplacement = (span, anchor) => {
 }
 
 /**
- * Finds redLinks in a document or document fragment
- * @param {!(Document|DocumentFragment)} content Document or fragment in which to seek redLinks
- * @return {!NodeList} Nodelist of zero or more redLink anchors
+ * Finds redLinks in a document or document fragment.
+ * @param {!(Document|DocumentFragment)} content Document or fragment in which to seek redLinks.
+ * @return {!NodeList} Nodelist of zero or more redLink anchors.
  */
 const redLinkAnchorsInContent = content => content.querySelectorAll('a.new')
 
 /**
- * Makes span to be used as cloning template for redLink anchor replacements
+ * Makes span to be used as cloning template for redLink anchor replacements.
  * @param  {!Document} document Document to use to create span element. Reminder: this can't be a
- * document fragment because fragments don't implement 'createElement'
+ * document fragment because fragments don't implement 'createElement'.
  * @return {!HTMLSpanElement} Span element suitable for use as template for redLink anchor
- * replacements
+ * replacements.
  */
 const spanToClone = document => document.createElement('span')
 
 /**
- * Replaces anchor with span
- * @param  {!HTMLAnchorElement} anchor Anchor element
- * @param  {!HTMLSpanElement} span Span element
+ * Replaces anchor with span.
+ * @param  {!HTMLAnchorElement} anchor Anchor element.
+ * @param  {!HTMLSpanElement} span Span element.
  * @return {void}
  */
 const replaceAnchorWithSpan = (anchor, span) => anchor.parentNode.replaceChild(span, anchor)
 
 /**
  * Hides redLink anchors in either a document or a document fragment so they are unclickable and
- * unfocusable
- * @param {!Document} document Document in which to hide redLinks
+ * unfocusable.
+ * @param {!Document} document Document in which to hide redLinks.
  * @param {?DocumentFragment} fragment If specified, redLinks are hidden in the fragment and the
- * document is used only for span cloning
+ * document is used only for span cloning.
  * @return {void}
  */
 const hideRedLinks = (document, fragment) => {
