6be7ba4365d3faa7215adec87a52c7b0c862331a
montehurd <montehurd@gmail.com>
2017-07-13 18:03:57 -0700

    Fix typo.

--- src/transform/FooterLegal.js
+++ src/transform/FooterLegal.js
@@ -11,28 +11,28 @@ import './FooterLegal.css'
  * @param {?string} licenseString
  * @param {?string} licenseSubstitutionString
  * @param {!string} containerID
- * @param {?FooterLegalClickCallback} licenceLinkClickHandler
+ * @param {?FooterLegalClickCallback} licenseLinkClickHandler
  */
 const add =
-  (content, licenseString, licenseSubstitutionString, containerID, licenceLinkClickHandler) => {
+  (content, licenseString, licenseSubstitutionString, containerID, licenseLinkClickHandler) => {
     const container = content.querySelector(`#${containerID}`)
     const licenseStringHalves = licenseString.split('$1')
 
     container.innerHTML =
   `<div class='pagelib_footer_legal_contents'>
     <hr class='pagelib_footer_legal_divider'>
-    <span class='pagelib_footer_legal_licence'>
+    <span class='pagelib_footer_legal_license'>
       ${licenseStringHalves[0]}
-      <a class='pagelib_footer_legal_licence_link'>
+      <a class='pagelib_footer_legal_license_link'>
         ${licenseSubstitutionString}
       </a>
       ${licenseStringHalves[1]}
     </span>
   </div>`
 
-    container.querySelector('.pagelib_footer_legal_licence_link')
+    container.querySelector('.pagelib_footer_legal_license_link')
       .addEventListener('click', () => {
-        licenceLinkClickHandler()
+        licenseLinkClickHandler()
       }, false)
   }
 
