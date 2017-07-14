c22d1274e93b07bf56805b4bb35e25d2b60e0974
Stephen Niedzielski <stephen@niedzielski.com>
2017-07-14 12:17:51 -0500

    Chore: require valid JSDocs

--- src/transform/Polyfill.js
+++ src/transform/Polyfill.js
@@ -20,7 +20,7 @@ const matchesSelector = (el, selector) => {
 /**
  * @param {!Element} element
  * @param {!string} selector
- * @return {!Element[]}
+ * @return {!Array.<Element>}
  */
 const querySelectorAll = (element, selector) =>
   Array.prototype.slice.call(element.querySelectorAll(selector))
