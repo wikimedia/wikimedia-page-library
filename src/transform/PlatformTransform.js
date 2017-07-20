1f4e867e5f609b414612711d5f6237f2a271d31c
Stephen Niedzielski <stephen@niedzielski.com>
2017-07-20 15:28:57 -0500

    New: add platform transform
    Allow platform-specific CSS rules. This is required for dark mode
    support.
    
    Bug: T165415

---
+++ src/transform/PlatformTransform.js
@@ -0,0 +1,29 @@
+const CLASS = { ANDROID: 'pagelib-platform-android', IOS:  'pagelib-platform-ios' }
+
+// Regular expressions from https://phabricator.wikimedia.org/diffusion/EMFR/browse/master/resources/mobile.startup/browser.js;c89f371ea9e789d7e1a827ddfec7c8028a549c12.
+/**
+ * @param {!Window} window
+ * @return {!boolean} true if the user agent is Android, false otherwise.
+ */
+const isAndroid = window => /android/i.test(window.navigator.userAgent)
+
+/**
+ * @param {!Window} window
+ * @return {!boolean} true if the user agent is iOS, false otherwise.
+ */
+const isIOs = window => /ipad|iphone|ipod/i.test(window.navigator.userAgent)
+
+/**
+ * @param {!Window} window
+ * @return {void}
+ */
+const classify = window => {
+  const html = window.document.querySelector('html')
+  if (isAndroid(window)) { html.classList.add(CLASS.ANDROID) }
+  if (isIOs(window)) { html.classList.add(CLASS.IOS) }
+}
+
+export default {
+  CLASS,
+  classify
+}
--- src/transform/index.js
+++ src/transform/index.js
@@ -8,6 +8,7 @@ import FooterMenu from './FooterMenu'
 import FooterReadMore from './FooterReadMore'
 import LazyLoadTransform from './LazyLoadTransform'
 import LazyLoadTransformer from './LazyLoadTransformer'
+import PlatformTransform from './PlatformTransform'
 import Polyfill from './Polyfill'
 import RedLinks from './RedLinks'
 import ThemeTransform from './ThemeTransform'
@@ -15,18 +16,27 @@ import Throttle from './Throttle'
 import WidenImage from './WidenImage'
 
 export default {
+  // todo: rename CollapseTableTransform.
   CollapseTable,
   CompatibilityTransform,
+  // todo: rename Footer.ContainerTransform, Footer.LegalTransform, Footer.MenuTransform,
+  //       Footer.ReadMoreTransform.
   FooterContainer,
   FooterLegal,
   FooterMenu,
   FooterReadMore,
   LazyLoadTransform,
   LazyLoadTransformer,
+  PlatformTransform,
+  // todo: rename RedLinkTransform.
   RedLinks,
   ThemeTransform,
+  // todo: rename WidenImageTransform.
   WidenImage,
   test: {
-    ElementGeometry, ElementUtilities, Polyfill, Throttle
+    ElementGeometry,
+    ElementUtilities,
+    Polyfill,
+    Throttle
   }
 }
