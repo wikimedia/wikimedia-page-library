5dba346ae196276e4427f82e36daae39615bda2b
montehurd <montehurd@gmail.com>
2017-07-31 21:47:27 -0700

    New shorter 'displayName' for menu items.
    The previous commit made it easier to make a display name.

--- demo/DemoArticles/ArticleRef.js
+++ demo/DemoArticles/ArticleRef.js
@@ -12,6 +12,9 @@ class ArticleRef {
   fileName() {
     return `${this.lang}.${this.title}.${this.revision}.json`
   }
+  displayName() {
+    return `"${this.lang} > ${this.title}" ${this.revision}`
+  }
   url() {
     return `https://${this.lang}.wikipedia.org/w/api.php?action=mobileview&page=${encodeURI(this.title)}&revision=${this.revision}&format=json&noheadings=true&prop=sections%7Ctext%7Clastmodified%7Clastmodifiedby%7Clanguagecount%7Cid%7Cprotection%7Ceditable%7Cdisplaytitle%7Cthumb%7Cdescription%7Cimage%7Crevision%7Cnamespace&sectionprop=toclevel%7Cline%7Canchor%7Clevel%7Cnumber%7Cfromtitle%7Cindex&sections=all&thumbwidth=640`
   }
