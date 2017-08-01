01c4d4b64d7bfcb790013625726b5b88632a60c3
montehurd <montehurd@gmail.com>
2017-08-01 12:41:49 -0700

    Tweaks to them demo menu to accomodate longer demo article titles.

--- demo/DemoArticles/ArticleRef.js
+++ demo/DemoArticles/ArticleRef.js
@@ -13,7 +13,7 @@ class ArticleRef {
     return `${this.lang}.${this.title}.${this.revision}.json`
   }
   displayName() {
-    return `"${this.lang} > ${this.title}" ${this.revision}`
+    return `"${this.lang} > ${this.title}"`
   }
   url() {
     return `https://${this.lang}.wikipedia.org/w/api.php?action=mobileview&page=${encodeURI(this.title)}&revision=${this.revision}&format=json&noheadings=true&prop=sections%7Ctext%7Clastmodified%7Clastmodifiedby%7Clanguagecount%7Cid%7Cprotection%7Ceditable%7Cdisplaytitle%7Cthumb%7Cdescription%7Cimage%7Crevision%7Cnamespace&sectionprop=toclevel%7Cline%7Canchor%7Clevel%7Cnumber%7Cfromtitle%7Cindex&sections=all&thumbwidth=640`
