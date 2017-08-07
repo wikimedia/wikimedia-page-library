4027cb89e392f5193a055f87b73c4e972eabcbf7
montehurd <montehurd@gmail.com>
2017-08-07 12:02:00 -0700

    Simplify custom CSS rollup process.
    Moved the Transform CSS first by changing order in index.js instead of in the rollup plugin.
    
    Simplified the rollup plugin.
    
    Added CSS comments to the rollup plugin to make it clear where CSS comes from.

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,26 @@
 import babel from 'rollup-plugin-babel'
-import css from 'rollup-plugin-css-porter'
 import pkg from './package.json'
 
 const fs = require('fs')
 
 /**
- * Custom rollup plug-in which bundles all transforms' CSS in a single file.
+ * Rollup plug-in bundling CSS from multiple transform files into one file.
  * @param  {callback} options Rollup plug-in options func
  * @return {void}
  */
-const wikimediaPageLibraryTransformCSSRollupPlugin = (options = {}) => {
-  const cssBuffer = {}
+const cssBundle = (options = {}) => {
+  const buffer = []
   return {
     transform(code, id) {
       if (!options.fileSuffixPattern.test(id)) { return }
-      if (!Object.prototype.hasOwnProperty.call(cssBuffer, id) || cssBuffer[id] !== code) {
-        cssBuffer[id] = code
-      }
+      const relativeFilePath = id.substring(id.match(options.fileSuffixPattern).index)
+      buffer.push(`/* --- CSS from '${relativeFilePath}' --- */ \n\n ${code}`)
       return ''
     },
     onwrite(opts) {
-      if (!Object.keys(cssBuffer).length) { return }
-
-      const keys = Object.keys(cssBuffer)
-
-      // We want the theme transform to be first. This is because the theme transform CSS has to use
-      // some '!important' CSS modifiers to reliably set themes on elements which may contain inline
-      // styles. Moving it to the top of the file is necessary so other transforms can override
-      // these '!important' themes transform CSS bits if needed.
-      const first = 'ThemeTransform.css'
-      keys.sort((x,y) => {
-        if (x.indexOf(first) > 0) {
-          return -1
-        } else if (y.indexOf(first) > 0) {
-          return 1
-        }
-        return 0
-      })
-
-      const css = keys.map(key => cssBuffer[key]).join('\n\n')
-      fs.writeFile(
-        options.dest,
-        css, err => { if (err) { throw err } }
-      )
+      const output =
+        `/* --- Wikimedia Page Library CSS bundle --- */\n\n\n${buffer.join('\n\n\n\n\n')}`
+      fs.writeFile(options.destination, output, err => { if (err) { throw err } })
     }
   }
 }
@@ -54,16 +32,15 @@ export default {
   moduleName: 'pagelib',
   sourceMap: true,
   plugins: [
-    // Deliver all transform CSS as a single file.
-    wikimediaPageLibraryTransformCSSRollupPlugin({
-      // Only grab files which end in '/src/transform/<TRANSFORM_NAME>.css'
+    cssBundle({
+      // Grab files ending in '/src/transform/<TRANSFORM_NAME>.css'
       fileSuffixPattern: /\/src\/transform.*\.css$/,
-      dest: './build/wikimedia-page-library-transform.css'
+      destination: './build/wikimedia-page-library-transform.css'
     }),
-    css({
-      minified: false,
-      include: './src/override/**.css',
-      dest: './build/wikimedia-page-library-override.css'
+    cssBundle({
+      // Grab files ending in '/src/override/<TRANSFORM_NAME>.css'
+      fileSuffixPattern: /\/src\/override.*\.css$/,
+      destination: './build/wikimedia-page-library-override.css'
     }),
     babel({
       plugins: ['external-helpers']
