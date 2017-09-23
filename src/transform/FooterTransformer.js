3b279bd48cb53e618224932ec6b03954edb65421
Natalia Harateh <nambatee@users.noreply.github.com>
2017-09-23 02:53:44 +0200

    Add new params to footer transformer

--- src/transform/FooterTransformer.js
+++ src/transform/FooterTransformer.js
@@ -29,18 +29,21 @@ export default class {
    * @param {!string} license
    * @param {!string} licenseSubstitutionString
    * @param {!FooterLegalClickCallback} licenseLinkClickHandler
+   * @param {?string} viewInBrowserString
+   * @param {!FooterBrowserClickCallback} browserLinkClickHandler
    * @param {!TitlesShownHandler} titlesShownHandler
    * @param {!SaveButtonClickHandler} saveButtonClickHandler
    * @return {void}
    */
   add(window, container, baseURL, title, readMoreHeader, readMoreLimit, license,
-    licenseSubstitutionString, licenseLinkClickHandler, titlesShownHandler,
+    licenseSubstitutionString, licenseLinkClickHandler, viewInBrowserString,
+    browserLinkClickHandler, titlesShownHandler,
     saveButtonClickHandler) {
     this.remove(window)
     container.appendChild(FooterContainer.containerFragment(window.document))
 
     FooterLegal.add(window.document, license, licenseSubstitutionString, ID_LEGAL_CONTAINER,
-      licenseLinkClickHandler)
+      licenseLinkClickHandler, viewInBrowserString, browserLinkClickHandler)
 
     FooterReadMore.setHeading(readMoreHeader, ID_READ_MORE_HEADER, window.document)
     FooterReadMore.add(title, readMoreLimit, ID_READ_MORE_CONTAINER, baseURL,
