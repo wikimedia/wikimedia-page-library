38c52d3bebd7d08d2074128e271ca1579e8c1201
montehurd <montehurd@gmail.com>
2017-11-02 16:56:20 -0700

    Removed inefficient recursion from getTableheader.
    Also modified 2 tests which were only passing because Domino doesn't define 'innerText'.

--- src/transform/CollapseTable.js
+++ src/transform/CollapseTable.js
@@ -13,48 +13,20 @@ const SECTION_TOGGLED_EVENT_TYPE = 'section-toggled'
  * @return {!Array<string>}
  */
 const getTableHeader = (element, pageTitle) => {
-  let thArray = []
-
-  if (!element.children) {
-    return thArray
-  }
-
-  for (let i = 0; i < element.children.length; i++) {
-    const el = element.children[i]
-
-    if (el.tagName === 'TH') {
-      // ok, we have a TH element!
-      // However, if it contains more than two links, then ignore it, because
-      // it will probably appear weird when rendered as plain text.
-      const aNodes = el.querySelectorAll('a')
-      // todo: these conditionals are very confusing. Rewrite by extracting a
-      //       method or simplify.
-      if (aNodes.length < 3) {
-        // todo: remove nonstandard Element.innerText usage
+  const thArray = []
+  const headers = Polyfill.querySelectorAll(element, 'th')
+  if (headers) {
+    headers.forEach(header => {
+      const anchors = Polyfill.querySelectorAll(header, 'a')
+      if (anchors.length < 3) {
         // Also ignore it if it's identical to the page title.
-        if ((el.innerText && el.innerText.length || el.textContent.length) > 0
-                && el.innerText !== pageTitle && el.textContent !== pageTitle
-                && el.innerHTML !== pageTitle) {
-          thArray.push(el.innerText || el.textContent)
+        if ((header.textContent && header.textContent.length) > 0
+          && header.textContent !== pageTitle && header.innerHTML !== pageTitle) {
+          thArray.push(header.textContent)
         }
       }
-    }
-
-    // if it's a table within a table, don't worry about it
-    if (el.tagName === 'TABLE') {
-      continue
-    }
-
-    // todo: why do we need to recurse?
-    // recurse into children of this element
-    const ret = getTableHeader(el, pageTitle)
-
-    // did we get a list of TH from this child?
-    if (ret.length > 0) {
-      thArray = thArray.concat(ret)
-    }
+    })
   }
-
   return thArray
 }
 
