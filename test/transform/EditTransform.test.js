2b47c4620d56b5bd3fffc24738b3f5b4b1e74b3c
Stephen Niedzielski <stephen@niedzielski.com>
2018-04-20 14:11:36 -0500

    Chore: fix lints

--- test/transform/EditTransform.test.js
+++ test/transform/EditTransform.test.js
@@ -1,58 +1,57 @@
 import assert from 'assert'
-import domino from 'domino'
-import pagelib from '../../build/wikimedia-page-library-transform'
 import fixtureIO from '../utilities/FixtureIO'
+import pagelib from '../../build/wikimedia-page-library-transform'
 const editTransform = pagelib.EditTransform
-const elementUtilities = pagelib.test.ElementUtilities
 let document
-var element
+let element
+
 describe('EditTransform', () => {
-    beforeEach(() => {
+  beforeEach(() => {
     document = fixtureIO.documentFromFixtureFile('EditTransform.html')
-    element = editTransform.newEditSectionHeader(document, 0, 2, "Title")
-})
-describe('.newEditSectionHeader()', () => {
+    element = editTransform.newEditSectionHeader(document, 0, 2, 'Title')
+  })
+  describe('.newEditSectionHeader()', () => {
     it('returns a non null element', () => {
-    assert.notEqual(element, null);
-})
-it('creates h2 element', () => {
-    assert.ok(element.childNodes.item(0).nodeName=="H2");
-})
-it('has all required attributes', () => {
-    assert.ok(element.childNodes.item(0).hasAttribute('data-id'));
-})
-it('has child nodes', () => {
-    assert.ok(element.childNodes.item(0).hasChildNodes);
-})
-it('has edit container and pencil', () => {
-    assert.ok(element.innerHTML.includes("pagelib_edit_section_link"));
-assert.ok(element.innerHTML.includes("pagelib_edit_section_link_container"));
-})
-it('has desired title', () => {
-    var text=element.childNodes.item(0).textContent;
-assert.ok(text.includes("Title"));
-})
-})
+      assert.notEqual(element, null)
+    })
+    it('creates h2 element', () => {
+      assert.ok(element.childNodes.item(0).nodeName === 'H2')
+    })
+    it('has all required attributes', () => {
+      assert.ok(element.childNodes.item(0).hasAttribute('data-id'))
+    })
+    it('has child nodes', () => {
+      assert.ok(element.childNodes.item(0).hasChildNodes)
+    })
+    it('has edit container and pencil', () => {
+      assert.ok(element.innerHTML.includes('pagelib_edit_section_link'))
+      assert.ok(element.innerHTML.includes('pagelib_edit_section_link_container'))
+    })
+    it('has desired title', () => {
+      const text = element.childNodes.item(0).textContent
+      assert.ok(text.includes('Title'))
+    })
+  })
 })
 describe('EditTransform', () => {
-    beforeEach(() => {
+  beforeEach(() => {
     document = fixtureIO.documentFromFixtureFile('EditTransform.html')
-    element = editTransform.newEditSectionHeader(document, 0, 3, "Title")
-})
-describe('.newEditSectionHeader()', () => {
+    element = editTransform.newEditSectionHeader(document, 0, 3, 'Title')
+  })
+  describe('.newEditSectionHeader()', () => {
     it('creates h3 element', () => {
-    assert.ok(element.childNodes.item(0).nodeName=="H3");
-})
-})
+      assert.ok(element.childNodes.item(0).nodeName === 'H3')
+    })
+  })
 })
 describe('EditTransform', () => {
-    beforeEach(() => {
+  beforeEach(() => {
     document = fixtureIO.documentFromFixtureFile('EditTransform.html')
-    element = editTransform.newEditSectionHeader(document, 0, 4, "Title")
-})
-describe('.newEditSectionHeader()', () => {
+    element = editTransform.newEditSectionHeader(document, 0, 4, 'Title')
+  })
+  describe('.newEditSectionHeader()', () => {
     it('creates h4 element', () => {
-    assert.ok(element.childNodes.item(0).nodeName=="H4");
-})
-})
+      assert.ok(element.childNodes.item(0).nodeName === 'H4')
+    })
+  })
 })
