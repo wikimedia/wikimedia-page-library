6aee95d2b0ea08d107f12164c2ce100157b3f6c1
BearND <bsitzmann@wikimedia.org>
2018-09-21 18:20:28 -0600

    Move header styles to CSS file
    Dropped color parameter in makeSvgElement since the same value was used
    and it made it easier to move those styles to the CSS file.
    
    Had to bump the maxSize for wikimedia-page-library-transform.css

--- src/transform/HeaderTransform.js
+++ src/transform/HeaderTransform.js
@@ -1,27 +1,24 @@
-const OPACITY_LIGHT = 0.54
-const OPACITY_DARK = 0.7
+import './HeaderTransform.css'
 
 /**
  * Makes an SVG element.
  * @param {!Document} document
  * @param {!number} width
  * @param {!number}height
- * @param {!string} color
  * @param {!string} svgContents
  * @return {!Element}
  */
-const makeSvgElement = (document, width, height, color, svgContents) => {
+const makeSvgElement = (document, width, height, svgContents) => {
   const svg = document.createElement('svg')
   svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
   svg.setAttribute('width', width)
   svg.setAttribute('height', height)
-  svg.setAttribute('style', `vertical-align:middle;fill:${color};`)
   svg.innerHTML = svgContents
   return svg
 }
 
 /**
- * Add header with title, pronunciation button, and description.
+ * Adds page header with title, pronunciation button, and description.
  * @param {!Document} document
  * @param {!Element} container
  * @param {!string} displayTitle
@@ -49,16 +46,14 @@ const add = (document, container, displayTitle, descriptionEditable, description
 
   if (hasPronunciation) {
     // Create and append the audio "speaker" icon
-    const audioIcon = makeSvgElement(document, 16, 16, 'currentColor',
+    const audioIcon = makeSvgElement(document, 16, 16,
       // eslint-disable-next-line max-len
       '<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path><path d="M0 0h24v24H0z" fill="none"></path>')
     audioIcon.setAttribute('viewBox', '0 0 24 24')
     audioIcon.style.margin = '0 6px 2px 6px'
-    if (isRtl) {
-      audioIcon.style.transform = 'scaleX(-1)'
-    }
     const audioAnchor = document.createElement('a')
     audioAnchor.setAttribute('data-action', 'pronunciation_click')
+    audioAnchor.setAttribute('class', 'pagelib_pronunciation')
     audioAnchor.setAttribute('style', 'color:inherit;')
     audioAnchor.innerHTML = audioIcon.outerHTML
     h1.appendChild(audioAnchor)
@@ -69,30 +64,21 @@ const add = (document, container, displayTitle, descriptionEditable, description
   const mainEditPencilAnchor = document.createElement('a')
   mainEditPencilAnchor.setAttribute('data-action', 'edit_main')
   mainEditPencilAnchor.setAttribute('class', 'pagelib_edit_section_link')
-  let style = isDarkMode ? 'filter:invert(100%);-webkit-filter:invert(100%);' : ''
-  style += isRtl ? 'float:left;transform:scaleX(-1);' : 'float:right;'
-  mainEditPencilAnchor.setAttribute('style', style)
-  mainEditPencilAnchor.style.opacity = isDarkMode ? OPACITY_DARK : OPACITY_LIGHT
 
   // Span that will contain the description and/or the two-line icon
   const descriptionSpan = document.createElement('span')
-  descriptionSpan.setAttribute('style', 'font-size:90%;')
+  descriptionSpan.setAttribute('class', 'pagelib_header_description')
   if (description) {
     descriptionSpan.innerHTML = description
-    descriptionSpan.style.opacity = isDarkMode ? OPACITY_DARK : OPACITY_LIGHT
   } else if (descriptionEditable) {
     const descriptionAnchor = document.createElement('a')
-    const descSvg = makeSvgElement(document, 24, 16, 'currentColor',
+    const descSvg = makeSvgElement(document, 24, 16,
       // eslint-disable-next-line max-len
       '<defs><path id="a" d="M0 0h24v24H0V0z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path d="M4 9h16v2H4zm0 4h10v2H4z" clip-path="url(#b)"/>')
     descSvg.setAttribute('viewBox', '4 9 24 8')
-    if (isRtl) {
-      descSvg.style.transform = 'scaleX(-1)'
-    }
     descriptionAnchor.setAttribute('href', '#')
     descriptionAnchor.setAttribute('data-action', 'edit_description')
     descriptionAnchor.innerHTML = descSvg.outerHTML + stringAddDescription
-    descriptionAnchor.setAttribute('style', 'font-style:italic;')
     descriptionSpan.appendChild(descriptionAnchor)
   } else {
     descriptionSpan.innerHTML = ' '
@@ -100,9 +86,7 @@ const add = (document, container, displayTitle, descriptionEditable, description
 
   // Decorative divider that appears below the title and description
   const dividerLine = document.createElement('div')
-  dividerLine.setAttribute('style',
-    'width:56px;border-top: 1px solid currentColor !important;margin-top:12px;margin-bottom:16px;')
-  dividerLine.style.opacity = isDarkMode ? OPACITY_DARK : OPACITY_LIGHT
+  dividerLine.setAttribute('class', 'pagelib_divider')
 
   descriptionDiv.appendChild(mainEditPencilAnchor)
   descriptionDiv.appendChild(descriptionSpan)
