ba19817a59adee20a9c62ffe02b3a213d7a56394
montehurd <mhurd@wikimedia.org>
2018-11-09 19:27:29 -0800

    new edit tranform method for article header
    article header turned out to be simpler to implement as a special case of a section header, which the edit tranform already supported

--- src/transform/EditTransform.js
+++ src/transform/EditTransform.js
@@ -8,6 +8,12 @@ const CLASS = {
   PROTECTION: { UNPROTECTED: '', PROTECTED: 'page-protected', FORBIDDEN: 'no-editing' }
 }
 
+const IDS = {
+  TITLE_DESCRIPTION: 'pagelib_edit_section_title_description',
+  ADD_TITLE_DESCRIPTION: 'pagelib_edit_section_add_title_description',
+  DIVIDER: 'pagelib_edit_section_divider'
+}
+
 const DATA_ATTRIBUTE = { SECTION_INDEX: 'data-id', ACTION: 'data-action' }
 const ACTION_EDIT_SECTION = 'edit_section'
 
@@ -47,9 +53,10 @@ const newEditSectionButton = (document, index) => {
  * @param {!number} level The *one-based* header or table of contents level.
  * @param {?string} titleHTML Title of this section header.
  * @param {?boolean} showEditPencil Whether to show the "edit" pencil (default is true).
+ * @param {?string} anchor Section anchor used for jumping between sections.
  * @return {!HTMLElement}
  */
-const newEditSectionHeader = (document, index, level, titleHTML, showEditPencil = true) => {
+const newEditSectionHeader = (document, index, level, titleHTML, showEditPencil = true, anchor) => {
   const element = document.createElement('div')
   element.className = CLASS.SECTION_HEADER
 
@@ -64,11 +71,63 @@ const newEditSectionHeader = (document, index, level, titleHTML, showEditPencil 
     element.appendChild(button)
   }
 
+  if (anchor && anchor.length > 0) {
+    // TODO: consider renaming this 'id' to 'anchor' for clarity - would need to update native
+    // code as well - used when TOC sections made to jump to sections.
+    element.id = anchor
+  }
+
   return element
+}
+
+/**
+ * Lead section header is a special case as it needs to show article title and description too,
+ * and in addition to the lead edit pencil, the description can also be editable.
+ * As a client, you may wish to set the ID attribute.
+ * @param {!Document} document
+ * @param {?string} articleDisplayTitle Article display title.
+ * @param {?string} titleDescription Article title description.
+ * @param {?string} addTitleDescriptionString Localized string e.g. 'Add title description'.
+ * @param {?boolean} isTitleDescriptionEditable Whether title description is editable.
+ * @param {?boolean} showEditPencil Whether to show the "edit" pencil (default is true).
+ * @param {?string} anchor Section anchor used for jumping between sections.
+ * @return {!HTMLElement}
+ */
+const newEditLeadSectionHeader = (document, articleDisplayTitle, titleDescription,
+  addTitleDescriptionString, isTitleDescriptionEditable, showEditPencil = true, anchor) => {
+
+  const container = document.createDocumentFragment()
+
+  const header = newEditSectionHeader(document, 0, 1, articleDisplayTitle, showEditPencil, anchor)
+  container.appendChild(header)
+
+  const titleDescriptionExists = titleDescription !== undefined && titleDescription.length > 0
+  if (!isTitleDescriptionEditable || titleDescriptionExists) {
+    const p = document.createElement('p')
+    p.id = IDS.TITLE_DESCRIPTION
+    p.innerHTML = titleDescription
+    container.appendChild(p)
+  } else {
+    const a = document.createElement('a')
+    a.href = '#'
+    a.setAttribute('data-action', 'add_title_description')
+    const p = document.createElement('p')
+    p.id = IDS.ADD_TITLE_DESCRIPTION
+    p.innerHTML = addTitleDescriptionString
+    a.appendChild(p)
+    container.appendChild(a)
+  }
+
+  const divider = document.createElement('hr')
+  divider.id = IDS.DIVIDER
+  container.appendChild(divider)
+
+  return container
 }
 
 export default {
   CLASS,
   newEditSectionButton,
-  newEditSectionHeader
+  newEditSectionHeader,
+  newEditLeadSectionHeader
 }
