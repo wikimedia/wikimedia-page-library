eae5248db583090045acbfd417354a75b19f8101
montehurd <mhurd@wikimedia.org>
2018-11-14 18:26:09 -0800

    add pronunciation icon

--- src/transform/EditTransform.js
+++ src/transform/EditTransform.js
@@ -11,11 +11,14 @@ const CLASS = {
 const IDS = {
   TITLE_DESCRIPTION: 'pagelib_edit_section_title_description',
   ADD_TITLE_DESCRIPTION: 'pagelib_edit_section_add_title_description',
-  DIVIDER: 'pagelib_edit_section_divider'
+  DIVIDER: 'pagelib_edit_section_divider',
+  PRONUNCIATION: 'pagelib_edit_section_title_pronunciation'
 }
 
 const DATA_ATTRIBUTE = { SECTION_INDEX: 'data-id', ACTION: 'data-action' }
 const ACTION_EDIT_SECTION = 'edit_section'
+const ACTION_TITLE_PRONUNCIATION = 'title_pronunciation'
+const ACTION_ADD_TITLE_DESCRIPTION = 'add_title_description'
 
 /**
  * @param {!Document} document
@@ -91,14 +94,24 @@ const newEditSectionHeader = (document, index, level, titleHTML, showEditPencil 
  * @param {?boolean} isTitleDescriptionEditable Whether title description is editable.
  * @param {?boolean} showEditPencil Whether to show the "edit" pencil (default is true).
  * @param {?string} anchor Section anchor used for jumping between sections.
- * @return {!HTMLElement}
+ * @param {?boolean} hasPronunciation Whether to show pronunciation speaker icon (default is false).
+* @return {!HTMLElement}
  */
 const newEditLeadSectionHeader = (document, articleDisplayTitle, titleDescription,
-  addTitleDescriptionString, isTitleDescriptionEditable, showEditPencil = true, anchor) => {
+  addTitleDescriptionString, isTitleDescriptionEditable, showEditPencil = true, anchor,
+  hasPronunciation = false) => {
 
   const container = document.createDocumentFragment()
 
   const header = newEditSectionHeader(document, 0, 1, articleDisplayTitle, showEditPencil, anchor)
+
+  if (hasPronunciation) {
+    const a = document.createElement('a')
+    a.setAttribute(DATA_ATTRIBUTE.ACTION, ACTION_TITLE_PRONUNCIATION)
+    a.id = IDS.PRONUNCIATION
+    header.querySelector('h1').appendChild(a)
+  }
+
   container.appendChild(header)
 
   const titleDescriptionExists = titleDescription !== undefined && titleDescription.length > 0
@@ -110,7 +123,7 @@ const newEditLeadSectionHeader = (document, articleDisplayTitle, titleDescriptio
   } else {
     const a = document.createElement('a')
     a.href = '#'
-    a.setAttribute('data-action', 'add_title_description')
+    a.setAttribute(DATA_ATTRIBUTE.ACTION, ACTION_ADD_TITLE_DESCRIPTION)
     const p = document.createElement('p')
     p.id = IDS.ADD_TITLE_DESCRIPTION
     p.innerHTML = addTitleDescriptionString
