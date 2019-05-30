9a68ad70e21c1740abd0c344f063458e1ee0e685
berndsi <besitzus@gmail.com>
2019-05-29 22:57:08 -0600

    add platform and clientVersion settings
    * clientVersion is not used yet but it seems good practice to ask
    clients to use that from the start.[1]
    
    * PlatformTransform is used to have client identify themselves.[1]
    Another advantage of this is that this doesn't require a window object.
    Added setPlatform() to PlatformTransform.
    
    [1] https://brightinventions.pl/blog/8-steps-to-keep-your-api-sane/

--- src/pcs/c1/index.js
+++ src/pcs/c1/index.js
@@ -1,8 +1,10 @@
 import PageMods from './PageMods'
+import Platforms from './Platforms'
 import Scroller from './Scroller'
 import Themes from './Themes'
 
 export default {
+  Platforms,
   PageMods,
   Scroller,
   Themes
