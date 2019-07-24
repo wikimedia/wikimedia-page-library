56692dfdd0e421de994c3a9fff8df79f6bc17399
Joe Walsh <joewalsh@users.noreply.github.com>
2019-07-24 10:52:45 -0400

    add getCurrentSection

--- src/transform/SectionUtilities.ts
+++ src/transform/SectionUtilities.ts
@@ -23,7 +23,45 @@ const getSectionOffsets = (body: HTMLBodyElement): object => {
     }
 }
 
+/**
+ * get current section
+ * @param  {!Window} window object.
+ * @return {!string} section id
+ */
+const getCurrentSection = (window: Window): string => {
+    const document = window.document;
+    const sectionHeaders = document.querySelectorAll( ".section_heading, .pagelib_edit_section_header" );
+    if (!sectionHeaders) {
+        return '-1';
+    }
+    const bottomDiv = document.getElementById( "bottom_stopper" );
+    if (!bottomDiv) {
+        return '-1';
+    }
+    const topCutoff = window.scrollY + ( document.documentElement.clientHeight / 2 );
+    if (topCutoff > bottomDiv.offsetTop) {
+        return '-1';
+    }
+    let curClosest = null;
+    for ( let i = 0; i < sectionHeaders.length; i++ ) {
+        const el = <HTMLElement>sectionHeaders[i];
+        if ( curClosest === null ) {
+            curClosest = el;
+            continue;
+        }
+        if ( el.offsetTop >= topCutoff ) {
+            break;
+        }
+        if ( Math.abs(el.offsetTop - topCutoff) < Math.abs(curClosest.offsetTop - topCutoff) ) {
+            curClosest = el;
+        }
+    }
+    const closestDataId = curClosest && curClosest.getAttribute( "data-id" )
+    return closestDataId || '0';
+}
+
 
 export default {
+    getCurrentSection,
     getSectionOffsets
 }
